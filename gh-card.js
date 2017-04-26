1fab8826649491cf898827750af5d692499d9557
z-shan <zeeshan2ahmed@gmail.com>
2017-04-26 18:34:20 -0500

    add react type styling

--- gh-card.js
+++ gh-card.js
@@ -1,9 +1,11 @@
 const Card = (props) => {
 	return (
-  	<div>
+  	<div style={{margin: '1em'}}>
     	<img src="http://placehold.it/75" />
-      <div>
-      	<div>Name here</div>
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+      	<div style={{fontSize: '1.25em', fontWeight: 'bold'}}>
+        	Name here
+        </div>
         <div>Company Name here ..</div>
     	</div>
     </div>
