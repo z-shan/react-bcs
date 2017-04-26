71f1ded3438ab1c8823bae8a8e517f14625870b2
z-shan <zeeshan2ahmed@gmail.com>
2017-04-26 18:40:48 -0500

    add cardlist component

--- gh-card.js
+++ gh-card.js
@@ -1,15 +1,26 @@
 const Card = (props) => {
 	return (
   	<div style={{margin: '1em'}}>
-    	<img src="http://placehold.it/75" />
+    	<img width="75" src={props.avatar_url} />
       <div style={{display: 'inline-block', marginLeft: 10}}>
       	<div style={{fontSize: '1.25em', fontWeight: 'bold'}}>
-        	Name here
+        	{props.name}
         </div>
-        <div>Company Name here ..</div>
+        <div>{props.company}</div>
     	</div>
     </div>
   );
 };
 
-ReactDOM.render(<Card />, mountNode);
+const CardList = (props) => {
+	return (
+  	<div>
+    	<Card name="mojombo"
+      	avatar_url="https://avatars3.githubusercontent.com/u/1?v=3"
+        company="Facebook"
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<CardList />, mountNode);
