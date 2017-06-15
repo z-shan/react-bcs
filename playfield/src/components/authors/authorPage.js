1d3c83db836ab025586b0c654228adb3e973d742
z-shan <zeeshan2ahmed@gmail.com>
2017-06-15 06:26:41 +0300

    add prop validation

--- playfield/src/components/authors/authorPage.js
+++ playfield/src/components/authors/authorPage.js
@@ -2,8 +2,9 @@
 
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
 
-var Authors = React.createClass({
+var AuthorPage = React.createClass({
     
     getInitialState: function() {
         return {
@@ -11,38 +12,22 @@ var Authors = React.createClass({
         };
     },
 
-    componentWillMount: function() {
-        this.setState({authors: AuthorApi.getAllAuthors()});
+    componentDidMount: function() {
+        if(this.isMounted()) {
+            this.setState({authors: AuthorApi.getAllAuthors()});
+        }
     },
     
     
     render: function() {
-        var createAuthorRow = function(author) {
-            return (
-                <tr key={author.id}>
-                    <td><a href={'/#authors/' + author.id}>{author.id}</a></td>
-                    <td>{author.firstName} {author.lastName}</td>
-                </tr>
-            );
-        };
-
         return (
             <div>
                 <h1>Authors</h1>
-
-                <table className="table">
-                    <thead>
-                        <th>ID</th>
-                        <th>Name</th>    
-                    </thead>
-                    <tbody>
-                        {this.state.authors.map(createAuthorRow, this)}
-                    </tbody>
-                </table>
+                <AuthorList authors={this.state.authors} />
             </div>
 
         );
     }
 });
 
-module.exports = Authors;
+module.exports = AuthorPage;
