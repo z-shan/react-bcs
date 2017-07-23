476bc04f8950cdaa02c602638efed3f42757efd2
z-shan <zeeshan2ahmed@gmail.com>
2017-07-23 15:39:30 -0500

    completed form

--- playfield/src/components/authors/authorForm.js
+++ playfield/src/components/authors/authorForm.js
@@ -1,26 +1,39 @@
 'use strict';
 
 var React = require('react');
+var Input = require('../common/textInput');
 
 var AuthorForm = React.createClass({
     
+    propTypes: {
+        author: React.PropTypes.object.isRequired,
+        onSave: React.PropTypes.func.isRequired,
+        onChange: React.PropTypes.func.isRequired,
+        errors: React.PropTypes.object
+    },
     
     render: function() {
         return (
             <form>
                 <h1>Manage Author</h1>
-                <label htmlFor="firstName">First Name</label>
-                <input type="text" name="firstName" className="form-control"
-                    ref="firstName" placeholder="First Name" value={this.props.author.firstName} 
-                    onChange={this.props.onChange}/>
-                <br />
+                <Input
+                    name="firstName"
+                    label="First Name"
+                    value={this.props.author.firstName}
+                    onChange={this.props.onChange}
+                    error={this.props.errors.firstName} />
+                
+                <Input
+                    name="lastName"
+                    label="Last Name"
+                    value={this.props.author.lastName}
+                    onChange={this.props.onChange} 
+                    error={this.props.errors.lastName} />
 
-                <label htmlFor="lastName">Last Name</label>
-                <input type="text" name="lastName" className="form-control"
-                    ref="lastName" placeholder="Last Name" value={this.props.author.lastName} 
-                    onChange={this.props.onChange}/>
-                <br />
-                <input type="submit" value="Save" className="btn btn-default" />
+                <input type="submit" 
+                    value="Save" 
+                    className="btn btn-default"
+                    onClick={this.props.onSave}/>
             </form>
         );
     }
