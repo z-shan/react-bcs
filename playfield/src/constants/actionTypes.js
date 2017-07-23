3d6f2c9448eb420874a72cf53ca4f775fc90471d
z-shan <zeeshan2ahmed@gmail.com>
2017-07-23 17:59:04 -0500

    added base flux implementation

---
+++ playfield/src/constants/actionTypes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var keyMirror = require('react/lib/keyMirror'); //copies key to value
+
+module.exports = keyMirror({
+    CREATE_AUTHOR: null
+});
---
+++ playfield/src/stores/authorStore.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var CHANGE_EVENT = 'change';
+
+// take empty obj, extend it with eventemitter
+var AuthorStore = assign({}, EventEmitter.prototype, {
+    addChangeListener: function(callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+
+    removeChangeListener: function(callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    },
+
+    emitChange: function() {
+        this.emit(CHANGE_EVENT);
+    }
+});
+
+Dispatcher.register(function(action) {
+    switch(action.ActionType) {
+        
+    }
+});
+
+module.exports = AuthorStore;
